38b325d54349e4a91d2710d66ad3a0ff533edc95
Maxim Sukharev <max@smacker.ru>
2019-05-27 17:57:46 +0200

    generate diff from samples as well

---
+++ testdata/process_samples.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -e
+
+BIN=/Users/smacker/Downloads/gumtree-2.1.2/bin/gumtree
+
+mkdir -p parsed
+for f in `find ./samples -type f -name "*_v[01].java"`; do
+    echo "parsing $f"
+    mkdir -p `dirname ./parsed/$f`
+    $BIN parse $f > ./parsed/$f
+done
+
+for f in `find ./samples -type f -name "*_v0.java"`; do
+    out="./parsed/${f/_v0\.java/_diff.java}"
+    echo "generating diff $out"
+    $BIN jsondiff $f "${f/_v0/_v1}" > $out
+done
