1e22acf9efac82866f059dcfd84b0c4abf707b3d
Maxim Sukharev <max@smacker.ru>
2019-07-01 16:05:05 +0200

    allow to run process_samples.sh from anywhere

--- testdata/process_samples.sh
+++ testdata/process_samples.sh
@@ -2,17 +2,21 @@
 
 set -e
 
-BIN=/Users/smacker/Downloads/gumtree-2.1.2/bin/gumtree
+BIN=${JAVA_GUM_BIN:-/Users/smacker/Downloads/gumtree-2.1.2/bin/gumtree}
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-mkdir -p parsed
-for f in `find ./samples -type f -name "*_v[01].java"`; do
-    echo "parsing $f"
-    mkdir -p `dirname ./parsed/$f`
-    $BIN parse $f > ./parsed/$f
-done
 
-for f in `find ./samples -type f -name "*_v0.java"`; do
-    out="./parsed/${f/_v0\.java/_diff.java}"
-    echo "generating diff $out"
-    $BIN jsondiff $f "${f/_v0/_v1}" > $out
-done
+(cd $DIR
+    mkdir -p parsed
+    for f in `find ./samples -type f -name "*_v[01].java"`; do
+        echo "parsing $f"
+        mkdir -p `dirname ./parsed/$f`
+        $BIN parse $f > ./parsed/$f
+    done
+
+    for f in `find ./samples -type f -name "*_v0.java"`; do
+        out="./parsed/${f/_v0\.java/_diff.java}"
+        echo "generating diff $out"
+        $BIN jsondiff $f "${f/_v0/_v1}" > $out
+    done
+)
